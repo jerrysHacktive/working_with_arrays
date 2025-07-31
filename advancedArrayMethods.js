4aaee7d1c8975ac89eb0c596e9c00b9c7a3a9d74
jerry <devygramm@gmail.com>
2025-07-31 01:13:40 +0100

    Update advancedArrayMethods.js

--- advancedArrayMethods.js
+++ advancedArrayMethods.js
@@ -16,21 +16,21 @@ console.log(foundUser);
 //B) FINDINDEX METHOD: returns the index of the first element in the array that satisfies the provided testing function, if no element is found or satisfies the condition it returns -1
 //REAL LIFE IMPLEMENTATION
 //1) find the index of the student with a spoecific name
-// const students = [
-//   { id: 1, name: "jerry" },
-//   { id: 2, name: "tope" },
-//   { id: 3, name: "austine" },
-// ];
+const students = [
+  { id: 1, name: "jerry" },
+  { id: 2, name: "tope" },
+  { id: 3, name: "austine" },
+];
 
-// const studentName = "jerry";
+const studentName = "jerry";
 
-// const index = students.findIndex((student) => student.name === studentName);
+const index = students.findIndex((student) => student.name === studentName);
 
-// if (index !== -1) {
-//   console.log(` student ${studentName} found at index ${index}`);
-// } else {
-//   console.log(` student ${studentName} not found`);
-// }
+if (index !== -1) {
+  console.log(` student ${studentName} found at index ${index}`);
+} else {
+  console.log(` student ${studentName} not found`);
+}
 
 // can also be used to remove or update an element in an array
 const numbers = [ 10, 20, 30, 40]
